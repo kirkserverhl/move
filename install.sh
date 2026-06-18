7ee687b2f383a9e03cc7812cf9304018ba91422e
kirkserverhl <kirkserverhl@gmail.com>
2026-06-17 21:10:36 -0400

    moved minimal images to wallpaper repro

--- install.sh
+++ install.sh
@@ -26,14 +26,6 @@ exec > >(tee -a "$LOGFILE") 2>&1
 # Welcome Screen
 # ============================================================
 clear
-
-echo "  ___ ___                       ________
- /   |   \ ___._______________ /  ____________ __ _____  __
-/    ~    <   |  \____ \_  __ /   \  __\_  __ |  |  \  \/ /
-\    Y    /\___  |  |_> |  | \\    \_\  |  | \|  |  /\   /
- \___|_  / / ____|   __/|__|   \______  |__|  |____/  \_/
-       \/  \/    |__|                 \/
-"
 echo ""
 log_status "Welcome to Hyprland Gruvbox Installation!"
 log_status "Logs will be saved to: $LOGFILE"
